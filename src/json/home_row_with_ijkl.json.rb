2f501f981c48a50a14c8e0cfe508d1e33f4ac197
Oleksandr Komendant <Oleksandr.Komendant@walmart.com>
2020-08-28 20:39:12 -0400

    Add home_row_with_ijkl.json.rb

---
+++ src/json/home_row_with_ijkl.json.rb
@@ -0,0 +1,128 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require_relative '../lib/karabiner.rb'
+require_relative '../lib/key_codes.rb'
+
+def main
+  arrow_mode = "arrow_mode"
+
+  puts JSON.pretty_generate(
+    "title" => "ASDF + IJKL as arrows with modifiers",
+    "rules" => [
+      {
+        "description" => "Change A/S/D/F/G + I/J/K/L/M/; to ⇧/⌃/⌥/⌘/<no modifier> + ↑/←/↓/→/⌫/⌦",
+        "manipulators" => [
+            generate_simultaneous_modifiers(["a", "s", "d", "f"], ["left_shift", "left_control", "left_option", "left_command"], arrow_mode),
+
+            generate_simultaneous_modifiers(["a", "s", "d"], ["left_shift", "left_control", "left_option"], arrow_mode),
+            generate_simultaneous_modifiers(["a", "s", "f"], ["left_shift", "left_control", "left_command"], arrow_mode),
+            generate_simultaneous_modifiers(["a", "d", "f"], ["left_shift", "left_option", "left_command"], arrow_mode),
+            generate_simultaneous_modifiers(["s", "d", "f"], ["left_control", "left_option", "left_command"], arrow_mode),
+
+            generate_simultaneous_modifiers(["a", "s"], ["left_shift", "left_control"], arrow_mode),
+            generate_simultaneous_modifiers(["a", "d"], ["left_shift", "left_option"], arrow_mode),
+            generate_simultaneous_modifiers(["a", "f"], ["left_shift", "left_command"], arrow_mode),
+            generate_simultaneous_modifiers(["s", "d"], ["left_control", "left_option"], arrow_mode),
+            generate_simultaneous_modifiers(["s", "f"], ["left_control", "left_command"], arrow_mode),
+            generate_simultaneous_modifiers(["d", "f"], ["left_option", "left_command"], arrow_mode),
+            
+            generate_rebind_if("i", "up_arrow", arrow_mode),
+            generate_rebind_if("j", "left_arrow", arrow_mode),
+            generate_rebind_if("k", "down_arrow", arrow_mode),
+            generate_rebind_if("l", "right_arrow", arrow_mode),
+
+            generate_simultaneous("a", "i", "up_arrow", ["left_shift"], arrow_mode),
+            generate_simultaneous("a", "j", "left_arrow", ["left_shift"], arrow_mode),
+            generate_simultaneous("a", "k", "down_arrow", ["left_shift"], arrow_mode),
+            generate_simultaneous("a", "l", "right_arrow", ["left_shift"], arrow_mode),  
+        ],
+      },
+    ],
+  )
+end
+
+def generate_simultaneous_modifiers(from_keys, to_keys, set_var)
+    {
+      "description" => "#{from_keys.join(" + ")} -> #{to_keys.join(" + ")}",
+      "type" => "basic",
+      "from" => {
+          "simultaneous" => KeyCodes.map(*from_keys),
+          "simultaneous_options" =>
+           {
+            "detect_key_down_uninterruptedly" => true,
+            "key_down_order" => "insensitive",
+            "key_up_order" => "insensitive",
+            "key_up_when" => "any",
+            "to_after_key_up" => [
+                Karabiner.set_variable(set_var, 0),
+            ]
+        }
+      },
+      "to" => [
+          Karabiner.set_variable(set_var, 1),
+          {
+            "key_code" => to_keys.first(),
+            "modifiers" => to_keys[1, 10],
+          },
+      ],
+    }
+end
+
+def generate_simultaneous(trigger_key, from_key, to_key, modifiers, set_var)
+    {
+      "description" => "#{trigger_key} + #{from_key} -> #{modifiers.join(" + ")} + #{to_key}",
+      "type" => "basic",
+      "from" => {
+          "simultaneous" => [
+            { "key_code" => trigger_key },
+            { "key_code" => from_key },
+          ],
+          "simultaneous_options" =>
+          {
+            "detect_key_down_uninterruptedly" => true,
+            "key_down_order" => "insensitive",
+            "key_up_order" => "insensitive",
+            "to_after_key_up" => [
+                Karabiner.set_variable(set_var, 0),
+            ],
+          },
+           "modifiers" => {
+            "optional" => ["any"]
+          },
+      },
+      "to" => [
+          Karabiner.set_variable(set_var, 1),
+          {
+            "key_code" => to_key,
+            "modifiers" => [
+                *modifiers
+            ]
+          },
+          
+      ],
+    }
+end
+
+def generate_rebind_if(from_key, to_key, if_var)
+  {
+    "description" => "#{from_key} -> #{to_key} when var is 1",  
+    "type" => "basic",
+      "from" => {
+        "key_code" => from_key,
+        "modifiers" => {
+            "optional" => ["any"]
+        },
+      },
+      "to" => [
+        {
+          "key_code" => to_key,
+        },
+      ],
+      "conditions" => [
+        Karabiner.variable_if(if_var, 1),
+      ]
+  }
+end
+
+main()
